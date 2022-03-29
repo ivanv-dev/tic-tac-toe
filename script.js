9c0c8a8f0aea6bf5218b1bcdd25876b6d4e8e600
ivan.vukicevic <ivan.vukicevic.dev@gmail.com>
2022-03-29 21:00:30 +0200

    tic tac toe initial logic

---
+++ script.js
@@ -0,0 +1,68 @@
+const Gameboard = (() => {
+const gameboard = ['x', null, null, null, null, null, null, null, null];
+
+const status = {
+    win: 'WIN',
+    draw: 'DRAW'
+}
+
+const isEveryFieldFilled = () => {
+    return gameboard.filter( sign => sign === null).length > 0
+}
+const checkHorizontal = () => {
+    if(gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2] && gameboard[1] !== null) return true;
+    else if(gameboard[3] === gameboard[4] && gameboard[4] === gameboard[5] && gameboard[4] !== null) return true;
+    else if(gameboard[6] === gameboard[7] && gameboard[7] === gameboard[8] && gameboard[7] !== null) return true;
+    return false;
+}
+
+const checkVertical = () => {
+    if(gameboard[0] === gameboard[3] && gameboard[3] === gameboard[6] && gameboard[3] != null) return true;
+    else if(gameboard[1] === gameboard[4] && gameboard[4] === gameboard[7] && gameboard[4] != null) return true;
+    else if(gameboard[2] === gameboard[5] && gameboard[5] === gameboard[8] && gameboard[5] != null) return true;
+    return false;
+}
+
+const checkDiagonal = () => {
+    if(gameboard[0] === gameboard[4] && gameboard[4] === gameboard[8] && gameboard[4] != null) return true;
+    else if(gameboard[2] === gameboard[4] && gameboard[4] === gameboard[6] && gameboard[4] != null) return true;
+    return false;
+}
+
+const getCurrentOutcome = () => {
+    if(checkHorizontal() || checkVertical() || checkDiagonal())  return status.win;
+    else if(isEveryFieldFilled()) return status.draw;
+}
+
+const play = (position, player) => {
+
+    position = position - 1;
+    if(gameboard[position] === null) {
+        gameboard[position] = player.sign;
+        console.log(gameboard, player, player.getScore())
+
+        if(getCurrentOutcome() === 'WIN') {
+            player.incrementScore();
+            console.log(gameboard, player, player.getScore())
+            console.log(`${player.username} won this party!`);
+    }
+        else if(getCurrentOutcome() === 'DRAW') `${player.username} played last party in this draw match!`
+    }
+    else throw Error('Cannot fill already filled tic tac toe field.');
+} 
+
+return {play}
+})();
+
+const Player = (username, sign) => {
+    let score = 0;
+    const incrementScore = () => {score++;};
+    const getScore = () => score;
+
+    return {username, sign, incrementScore, getScore};
+};
+
+const mike = Player('mike97', 'x');
+
+Gameboard.play(2, mike);
+Gameboard.play(3, mike);
