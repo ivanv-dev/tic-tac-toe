64b8e2011d2ff0e38d830eeab96a33b6b598b85f
ivan.vukicevic <ivan.vukicevic.dev@gmail.com>
2022-04-01 20:28:24 +0200

    choosing sign, won message, clear gameboard at end added

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const Gameboard = (() => {
-const gameboard = ['x', null, null, null, null, null, null, null, null];
+const gameboard = [null, null, null, null, null, null, null, null, null];
 
+const clear = () => {
+for (let index = 0; index < gameboard.length; index++) {
+    gameboard[index] = null;
+}
+}
 const status = {
     win: 'WIN',
     draw: 'DRAW'
 }
 
 const isEveryFieldFilled = () => {
-    return gameboard.filter( sign => sign === null).length > 0
+    return gameboard.filter( sign => sign === null).length === 0
 }
 const checkHorizontal = () => {
     if(gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2] && gameboard[1] !== null) return true;
@@ -32,6 +37,7 @@ const checkDiagonal = () => {
 const getCurrentOutcome = () => {
     if(checkHorizontal() || checkVertical() || checkDiagonal())  return status.win;
     else if(isEveryFieldFilled()) return status.draw;
+    return null;
 }
 
 const play = (position, player) => {
@@ -40,57 +46,24 @@ const play = (position, player) => {
     if(gameboard[position] === null) {
         gameboard[position] = player.sign;
         console.log(gameboard, player, player.getScore())
+        console.log(getCurrentOutcome(), 'hmm')
 
         if(getCurrentOutcome() === 'WIN') {
             player.incrementScore();
             console.log(gameboard, player, player.getScore())
-            console.log(`${player.username} won this party!`);
+            clear()
+            return `${player.username} won this party!`
+
     }
-        else if(getCurrentOutcome() === 'DRAW') `${player.username} played last party in this draw match!`
+        else if(getCurrentOutcome() === 'DRAW'){
+            clear()
+            return `${player.username} played last party in this draw match!`}
     }
     else throw Error('Cannot fill already filled tic tac toe field.');
 } 
 
 return {play}
 })();
-
-
-const DOMController = (() => {
-
-    const firstPlayerSignChoice = document.querySelector('.first-player-sign');
-    const secondPlayerSignChoice = document.querySelector('.second-player-sign');
-
-    const cells = document.querySelectorAll('.cell');
-    const firstPlayerSigns = document.querySelectorAll('.first .signs .sign');
-
-    chooseSign = (player) => {
-
-    }
-
-    const initiateCellsListener = (player) => {
-        cells.forEach(cell => {
-            cell.addEventListener('click', e => {
-                if(e.target.textContent == ''){
-                e.target.textContent = `${player.sign}`;
-                }
-            })
-        })
-    }
-
-    const setFirstPlayerSigns = (player) => {
-        firstPlayerSigns.forEach(sign => {
-            sign.addEventListener('click', e => {
-                console.log(e.target.textContent)
-                player.sign = e.target.textContent;
-                console.log(player)
-            })
-        })
-    }
-return {initiateCellsListener, setFirstPlayerSigns};
-})();
-
-
-
 
 const Player = (username, sign) => {
     let score = 0;
@@ -100,9 +73,74 @@ const Player = (username, sign) => {
     return {username, sign, incrementScore, getScore};
 };
 
+const firstPlayer = Player('mike97', null);
+const secondPlayer = Player('joe97', null);
 
-const mike = Player('mike97', 'x');
-Gameboard.play(2, mike);
-Gameboard.play(3, mike);
-DOMController.setFirstPlayerSigns(mike);
-DOMController.initiateCellsListener(mike);
+
+const DOMController = (() => {
+
+    const firstPlayerSignChoice = document.querySelector('.first-player-sign');
+    const secondPlayerSignChoice = document.querySelector('.second-player-sign');
+
+    const cells = document.querySelectorAll('.cell');
+    const firstPlayerSigns = document.querySelectorAll('.signs .sign');
+    const gameboard = document.querySelector('.gameboard');
+    const firstDescription = document.querySelector('.first p')
+    const secondDescription = document.querySelector('.second p');
+    const winMessage = document.querySelector('.winMessage')
+
+
+    chooseSign = (player) => {
+
+    }
+
+    const clearCells = () => {
+        cells.forEach(cell => {
+            cell.textContent = ''
+        })
+    }
+    const initiateCellsListener = () => {
+        let player = firstPlayer;
+        cells.forEach(cell => {
+            cell.addEventListener('click', e => {
+                if(e.target.textContent == ''){
+                e.target.textContent = `${player.sign}`;
+               let textMessage =  Gameboard.play(Number(e.target.id), player);
+               console.log(textMessage, 'aaa')
+               winMessage.textContent = textMessage
+               if(textMessage !== null && textMessage !== undefined) clearCells();
+
+                }
+                if(player.username === firstPlayer.username) player = secondPlayer;
+                else player = firstPlayer;
+            })
+        })
+    }
+
+    const setPlayerSign = () => {
+        firstPlayerSigns.forEach(sign => {
+            sign.addEventListener('click', e => {
+                if(firstPlayer.sign === null){firstPlayer.sign =  e.target.textContent.toLowerCase();
+                    if(firstPlayer.sign === 'x') secondPlayer.sign = 'o'
+                    else secondPlayer.sign = 'x'
+                    firstDescription.textContent = `${firstPlayer.username} is ${firstPlayer.sign}`
+                    secondDescription.textContent = `${secondPlayer.username} is ${secondPlayer.sign}`
+
+                }
+                if(firstPlayer.sign !== null && secondPlayer.sign !== null) gameboard.style.display = 'grid';
+
+            })
+        })
+    }
+return {initiateCellsListener, setPlayerSign};
+})();
+
+
+
+
+
+
+
+
+DOMController.setPlayerSign();
+DOMController.initiateCellsListener();
